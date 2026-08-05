900aeebe10f1452b5e02fc99fded331205e678d4
nishviprp <nishviprp@gmail.com>
2026-08-05 19:44:43 -0400

    Fix: Update base path to ornglobal_new

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ornglobal/',
+  base: '/ornglobal_new/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'docs',
